98d33d94f61ce9afed341b0c9498b21fa5182293
zaycevalexander <102805810+zaycevalexander@users.noreply.github.com>
2023-04-05 10:38:47 +0300

    Delete mail/mail directory

--- mail/mail/user/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from user.models import Email_to_sending, File, User
-
-admin.site.register(User)
-admin.site.register(Email_to_sending)
-admin.site.register(File)
--- mail/mail/user/import_file/__init__.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class ImportFromFile:
-
-    def __init__(self, model: models.Model, file: object):
-        self.model = model
-        self.file = file
